907b1a50e8b2cfa14a79f581d7b5286bc886a813
Aritra Chatterjee <AritraChatterjee@fico.com>
2014-11-24 14:04:50 -0800

    https://bugster.forgerock.org/jira/browse/OPENAM-4831 is fixed now. Updating documentation and code to reflect the change.

--- src/main/java/org/forgerock/openam/doc/jwt/bearer/Main.java
+++ src/main/java/org/forgerock/openam/doc/jwt/bearer/Main.java
@@ -36,13 +36,14 @@ import java.util.Date;
  */
 public final class Main {
 
-    private static String clientId      = "jwt-bearer-client";
+    private static String clientId = "jwt-bearer-client";
     private static String tokenEndpoint = null;
 
     /**
      * Use a JWT as a bearer token to get an OAuth 2.0 access token.
      *
-     * @param args Command line arguments: OpenAM-serverUrl
+     * @param args
+     *            Command line arguments: OpenAM-serverUrl
      */
     public static void main(String[] args) {
 
@@ -55,7 +56,8 @@ public final class Main {
         tokenEndpoint = serverUrl.replaceAll("/$", "") + "/oauth2/access_token";
 
         String jws = getJws(tokenEndpoint);
-        System.out.println("\nPOSTing the following as a JWT bearer token:\n" + jws);
+        System.out.println("\nPOSTing the following as a JWT bearer token:\n"
+                + jws);
         System.out.println();
 
         try {
@@ -67,22 +69,27 @@ public final class Main {
     }
 
     private static String getUsage() {
-        String publicKey = "-----BEGIN PUBLIC KEY-----\n"
-                + "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgPhln8MgYQGvhALa7PQZ\n"
-                + "gU4JSUA9qfFfbVpgdK8QuxbIxrsMmUP6aeNoXxw9FEySFhkVcMmj8BVyPJuXJPee\n"
-                + "2ov7YmzcPtOZLoFKOTFlawkfKEkhgShTDCWhzSSLOBKbXkzAExxueSxBU3lVaI57\n"
-                + "jLgkxuLfS3qTfyUzrYoq1pI8/qO+dbUtOYv26KotbPngAzqFTc2UIjcnpF+HzBoE\n"
-                + "g8ASOYdk2q46Xa4aBa6bO5tgMUluiYtJCo7nxG4pVpJUzANuYQ20xhZcgpAvbFO3\n"
-                + "lcBvARASAn/OyTCkDZpC3UcTp17dTKY6E42FGK7iuTcAgScRVvGu/LgiT7BeDstl\n"
-                + "WwIDAQAB\n"
-                + "-----END PUBLIC KEY-----";
+        String publicKey = "-----BEGIN CERTIFICATE-----\n"
+                + "MIIDETCCAfmgAwIBAgIEU8SXLjANBgkqhkiG9w0BAQsFADA5MRswGQYDVQQKExJvcGVuYW0uZXhh\n"
+                + "bXBsZS5jb20xGjAYBgNVBAMTEWp3dC1iZWFyZXItY2xpZW50MB4XDTE0MTAyNzExNTY1NloXDTI0\n"
+                + "MTAyNDExNTY1NlowOTEbMBkGA1UEChMSb3BlbmFtLmV4YW1wbGUuY29tMRowGAYDVQQDExFqd3Qt\n"
+                + "YmVhcmVyLWNsaWVudDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAID4ZZ/DIGEBr4QC\n"
+                + "2uz0GYFOCUlAPanxX21aYHSvELsWyMa7DJlD+mnjaF8cPRRMkhYZFXDJo/AVcjyblyT3ntqL+2Js\n"
+                + "3D7TmS6BSjkxZWsJHyhJIYEoUwwloc0kizgSm15MwBMcbnksQVN5VWiOe4y4JMbi30t6k38lM62K\n"
+                + "KtaSPP6jvnW1LTmL9uiqLWz54AM6hU3NlCI3J6Rfh8waBIPAEjmHZNquOl2uGgWumzubYDFJbomL\n"
+                + "SQqO58RuKVaSVMwDbmENtMYWXIKQL2xTt5XAbwEQEgJ/zskwpA2aQt1HE6de3UymOhONhRiu4rk3\n"
+                + "AIEnEVbxrvy4Ik+wXg7LZVsCAwEAAaMhMB8wHQYDVR0OBBYEFIuI7ejuZTg5tJsh1XyRopGOMBcs\n"
+                + "MA0GCSqGSIb3DQEBCwUAA4IBAQBM/+/tYYVIS6LvPl3mfE8V7x+VPXqj/uK6UecAbfmRTrPk1ph+\n"
+                + "jjI6nmLX9ncomYALWL/JFiSXcVsZt3/412fOqjakFVS0PmK1vEPxDlav1drnVA33icy1wORRRu5/\n"
+                + "qA6mwDYPAZSbm5cDVvCR7Lt6VqJ+D0V8GABFxUw9IaX6ajTqkWhldY77usvNeTD0Xc4R7OqSBrnA\n"
+                + "SNCaUlJogWyzhbFlmE9Ne28j4RVpbz/EZn0oc/cHTJ6Lryzsivf4uDO1m3M3kM/MUyXc1Zv3rqBj\n"
+                + "TeGSgcqEAd6XlGXY1+M/yIeouUTi0F1bk1rNlqJvd57Xb4CEq17tVbGBm0hkECM8\n"
+                + "-----END CERTIFICATE-----";
 
-        return "Usage: OpenAM-serverUrl\n\n"
-                + "Before trying this client, "
+        return "Usage: OpenAM-serverUrl\n\n" + "Before trying this client, "
                 + "configure a top-level realm OAuth 2.0 client profile\n"
                 + "with client_id: " + clientId + ", "
-                + "and Client JWT Bearer Public Key:\n\n" + publicKey
-                + "\n\n"
+                + "and Client JWT Bearer Public Key:\n\n" + publicKey + "\n\n"
                 + "Then to use this client, pass it the OpenAM Server URL\n"
                 + "such as http://openam.example.com:8080/openam";
     }
@@ -91,16 +98,14 @@ public final class Main {
         Date exp = new Date(System.currentTimeMillis() + 1000 * 60 * 5);
 
         JwtBuilderFactory jwtBuilderFactory = new JwtBuilderFactory();
-        return jwtBuilderFactory.jws(
-                new SigningManager().newRsaSigningHandler(getPrivateKey()))
-                .headers().alg(JwsAlgorithm.RS256).done()
-                .claims(jwtBuilderFactory.claims()
-                        .iss(clientId)
-                        .sub(clientId)
-                        .aud(Collections.singletonList(tokenEndpoint))
-                        .exp(exp)
-                        .build())
-                .build();
+        return jwtBuilderFactory
+                .jws(new SigningManager().newRsaSigningHandler(getPrivateKey()))
+                .headers()
+                .alg(JwsAlgorithm.RS256)
+                .done()
+                .claims(jwtBuilderFactory.claims().iss(clientId).sub(clientId)
+                        .aud(Collections.singletonList(tokenEndpoint)).exp(exp)
+                        .build()).build();
     }
 
     private static PrivateKey getPrivateKey() {
@@ -108,15 +113,18 @@ public final class Main {
 
         try {
             KeyStore keystore = KeyStore.getInstance("JKS");
-            keystore.load(Main.class.getResourceAsStream("/keystore.jks"), "changeit".toCharArray());
-            privateKey = (PrivateKey) keystore.getKey("self-signed", "changeit".toCharArray());
+            keystore.load(Main.class.getResourceAsStream("/keystore.jks"),
+                    "changeit".toCharArray());
+            privateKey = (PrivateKey) keystore.getKey("self-signed",
+                    "changeit".toCharArray());
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
         }
 
         if (privateKey == null) {
-            System.err.println("Failed to retrieve private key from keystore.jks.");
+            System.err
+                    .println("Failed to retrieve private key from keystore.jks.");
             System.exit(-1);
         }
 
@@ -126,22 +134,27 @@ public final class Main {
     private static void post(String idToken) throws Exception {
         URL token = new URL(tokenEndpoint);
 
-        HttpURLConnection connection = (HttpURLConnection) token.openConnection();
+        HttpURLConnection connection = (HttpURLConnection) token
+                .openConnection();
 
         connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("Content-Type",
+                "application/x-www-form-urlencoded");
         connection.setDoOutput(true);
 
         // This client is of type confidential, so authentication is required
         // according to http://tools.ietf.org/html/rfc6749#section-3.2.1
-        String clientAssertionType =
-                URLEncoder.encode("urn:ietf:params:oauth:client-assertion-type:jwt-bearer", "UTF-8");
-        String grantType = URLEncoder.encode("urn:ietf:params:oauth:grant-type:jwt-bearer", "UTF-8");
+        String clientAssertionType = URLEncoder.encode(
+                "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+                "UTF-8");
+        String grantType = URLEncoder.encode(
+                "urn:ietf:params:oauth:grant-type:jwt-bearer", "UTF-8");
         String data = "client_assertion_type=" + clientAssertionType
-                + "&client_assertion=" + idToken
-                + "&grant_type=" + grantType + "&assertion=" + idToken;
+                + "&client_assertion=" + idToken + "&grant_type=" + grantType
+                + "&assertion=" + idToken;
 
-        DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+        DataOutputStream dataOutputStream = new DataOutputStream(
+                connection.getOutputStream());
         dataOutputStream.writeBytes(data);
         dataOutputStream.flush();
         dataOutputStream.close();
@@ -150,9 +163,11 @@ public final class Main {
 
         BufferedReader input;
         if (responseCode == 200) {
-            input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            input = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
         } else {
-            input = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            input = new BufferedReader(new InputStreamReader(
+                    connection.getErrorStream()));
         }
 
         StringBuilder response = new StringBuilder();
